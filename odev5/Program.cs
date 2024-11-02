f28567ff0184c2d0766efdef14181801a8389423
Hasan Umut <55670137+hasanumutduyar@users.noreply.github.com>
2024-11-02 16:23:54 +0300

    Update

---
+++ odev5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı giriniz = ");
+        string girilenSayi = Console.ReadLine();
+
+        int basamakSayisi = BasamakSayisiniBul(girilenSayi);
+
+        Console.WriteLine("Girilen sayı " + basamakSayisi + " basamaklıdır.");
+
+    }
+
+
+    static int BasamakSayisiniBul(string sayi)
+    {
+        if (sayi.StartsWith("-")) { sayi = sayi.Substring(1); }
+        return sayi.Length;
+    }
+}
