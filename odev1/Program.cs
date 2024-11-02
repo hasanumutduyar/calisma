14ccf594c97a882caa4b7182807fa227349557fa
Hasan Umut <55670137+hasanumutduyar@users.noreply.github.com>
2024-11-02 13:29:00 +0300

    Proje dosyası ekle.

---
+++ odev1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("1. sayıyı girin = ");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2. sayıyı girin = ");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+int kare = (sayi1 * sayi1 + sayi2 * sayi2);
+
+Console.WriteLine($"İki sayının kareleri toplamı = {kare}");
---
+++ odev2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Bir sayı giriniz = ");
+
+int sayi = Convert.ToInt32(Console.ReadLine());
+int faktoriyel = 1;
+
+for (int i = sayi; i >= 2; i--)
+{
+    faktoriyel *= i; 
+}
+
+Console.WriteLine($"{sayi} sayısının faktöriyeli = {faktoriyel}");
