411e85056ce49079f04b84802d8cba531e8838d1
Hasan Umut <55670137+hasanumutduyar@users.noreply.github.com>
2024-11-02 15:09:01 +0300

    Update

---
+++ odev3/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("1 ile 100 arasındaki asal sayılar:");
+
+        for (int sayi = 2; sayi <= 100; sayi++)
+        {
+            bool kontrol = true;
+
+            // 2'den 'sayi - 1' kadar olan sayılarla bölünebilirliğini kontrol et
+            for (int i = 2; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    kontrol = false;
+                    break; // Eğer bir bölen bulursa döngüyü kır
+                }
+            }
+
+            // Eğer asal ise yazdır
+            if (kontrol)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
