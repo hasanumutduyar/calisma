ec82eead1eaf0c666ee5f896ede2144ce0c8362d
Hasan Umut <55670137+hasanumutduyar@users.noreply.github.com>
2024-11-02 16:04:49 +0300

    Update

---
+++ odev4/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir kelime girin: ");
+        string kelime = Console.ReadLine();
+
+        string tersKelime = TerstenYaz(kelime);
+
+        Console.WriteLine("Ters yazılmış kelime: " + tersKelime);
+    }
+
+    static string TerstenYaz(string kelime)
+    {
+        char[] karakterler = kelime.ToCharArray();
+        Array.Reverse(karakterler);
+        return new string(karakterler);
+    }
+}
